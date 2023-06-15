a7d225934994a388407140991e57cb4333f44cc3
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-15 23:21:37 +0530

    Backend -todo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
 import express from "express";
+import cors from "cors";
+
 const app = express();
+app.use(cors()); // allowing everyone.
 
 async function addrecord(req, res) {
   const uri = "mongodb://127.0.0.1:27017";
@@ -36,9 +39,56 @@ async function findAllMessage(req, res) {
   res.json(list);
 }
 
+function helloPost(req, res) {
+  let result = { opr: true };
+  res.json(result);
+}
+
+// NEW TODO API
+async function addTodo(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("project");
+  const messageColl = db.collection("todo");
+
+  let inputDoc = {
+    task: req.query.task,
+    description: req.query.description,
+  };
+  await messageColl.insertOne(inputDoc);
+
+  await client.close();
+
+  res.json({ opr: "success" });
+}
+
+async function addUserRecord(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("project");
+  const messageColl = db.collection("user");
+
+  let inputDoc = {
+    username: req.query.username,
+    password: req.query.password,
+    email: req.query.email,
+    mobile: req.query.mobile,
+  };
+  await messageColl.insertOne(inputDoc);
+
+  await client.close();
+
+  res.json({ opr: "success" });
+}
+
 // http://localhost:4000/addrecord
 app.get("/addrecord", addrecord);
 app.get("/findAll", findAllMessage);
+app.post("/hello", helloPost);
+app.get("/addtodo", addTodo);
+app.get("/adduser", addUserRecord);
 
 // http://localhost:4000/
 app.listen(4000);
